62faa1c99b0b6dd779da05fdc7d3b239c6e9ea87
Naoumi1214 <wada121400@gmail.com>
2020-01-22 12:15:55 +0900

    ブログ記事とニュースリリースの実装が完了した

--- functions.php
+++ functions.php
@@ -24,3 +24,37 @@ add_action('wp_enqueue_scripts', 'inputsjs');
  */
 add_theme_support('menus');
 register_nav_menu('header-navi','ヘッダーナビ');
+
+//カスタム投稿関係
+/**
+ * ニュースリリースのカスタム投稿設定
+ */
+function create_post_newsrelease() {
+    register_post_type( 'newsrelease', [ 
+        'labels' => [
+            'name'          => 'ニュースリリース',
+            'singular_name' => 'newsrelease',
+        ],
+        'public'        => true,  
+        'has_archive'   => false,
+        'menu_position' => 5, 
+        'show_in_rest'  => true,
+    ]);
+}
+add_action( 'init', 'create_post_newsrelease' );
+/**
+ * ブログ生地のカスタム投稿設定
+ */
+function create_post_blog() {
+    register_post_type( 'blog', [ 
+        'labels' => [
+            'name'          => 'ブログ',
+            'singular_name' => 'blog',
+        ],
+        'public'        => true,  
+        'has_archive'   => false,
+        'menu_position' => 5, 
+        'show_in_rest'  => true,
+    ]);
+}
+add_action( 'init', 'create_post_blog' );
--- index.php
+++ index.php
@@ -1,8 +1,97 @@
 <?php get_header(); ?>
 <div class="container">
-<?php
+    <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
+            //ブログ記事記事の表示部分
+            $args = [
+                'post_type' => 'blog',
+            ];
+            $my_query = new WP_Query($args);
+            if ($my_query->have_posts()) : ?>
+                <h1><?php echo 'ブログ'?></h1>
+                <ul class="list-unstyled">
+                    <?php
+                    while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+                        <li class="media">
+                            <a class="d-flex thumbnail" href="#">
+                                <?php
+                                if (has_post_thumbnail()) :
+                                    the_post_thumbnail('thumbnail');
+                                else :
+                                ?>
+                                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/pose_dance_ukareru_woman.png" alt="" srcset="">
+                                <?php
+                                endif; ?>
+                            </a>
+                            <div class="media-body">
+                                <h1>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h1>
+                                <p class="post-meta">
+                                    <span class="post-date"><?php the_date(); ?>&nbsp;</span>
+                                    <span class="post-category">カテゴリラ：<?php the_category(','); ?></span>
+                                </p>
+                                <?php the_excerpt(); ?>
+                                <p class="more-link">
+                                    <a href="<?php the_permalink(); ?>">続きを読む</a>
+                                </p>
+                            </div>
+                        </li>
+
+                    <?php
+                    endwhile; ?>
+                </ul>
+            <?php
+            endif;
+
+            //ニュースリリースの表示部分
+            $args = [
+                'post_type' => 'newsrelease',
+            ];
+            $my_query = new WP_Query($args);
+            if ($my_query->have_posts()) : ?>
+                <h1><?php echo 'ニュースリリース' ?></h1>
+                <ul class="list-unstyled">
+                    <?php
+                    while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+                        <li class="media">
+                            <a class="d-flex thumbnail" href="#">
+                                <?php
+                                if (has_post_thumbnail()) :
+                                    the_post_thumbnail('thumbnail');
+                                else :
+                                ?>
+                                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/pose_dance_ukareru_woman.png" alt="" srcset="">
+                                <?php
+                                endif; ?>
+                            </a>
+                            <div class="media-body">
+                                <h1>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h1>
+                                <p class="post-meta">
+                                    <span class="post-date"><?php the_date(); ?>&nbsp;</span>
+                                    <span class="post-category">カテゴリラ：<?php the_category(','); ?></span>
+                                </p>
+                                <?php the_excerpt(); ?>
+                                <p class="more-link">
+                                    <a href="<?php the_permalink(); ?>">続きを読む</a>
+                                </p>
+                            </div>
+                        </li>
+
+                    <?php
+                    endwhile; ?>
+                </ul>
+    <?php
+            endif;
         endwhile;
     endif;
     ?>
